f4c563ffdb0adf18a576052b38666a1f0c32c7a9
mohamedibourki <mohamedibourkioffice@gmail.com>
2023-11-02 23:44:27 +0100

    fixing somethings

--- script.js
+++ script.js
@@ -1,10 +1,17 @@
-let x = document.querySelectorAll(".window")
-let sit1 = document.querySelectorAll(".sit1")
-let sit2 = document.querySelectorAll(".sit2")
 
-x.forEach((e) => {
-    e.addEventListener("click", () => {
-        document.querySelector(".sit1").setAttribute("id", "vxx")
-        document.querySelector(".sit2").setAttribute("id", "vxx")
-    })
-})
+function myFunc(targetDiv) {
+    // Get a reference to all the div elements
+    var divs = document.querySelectorAll("div");
+
+    // Iterate through all the div elements
+    for (var i = 0; i < divs.length; i++) {
+        var currentDiv = divs[i];
+        if (currentDiv === targetDiv) {
+            // If the current div is the target div, add the 'big' class
+            currentDiv.classList.add("big");
+        } else {
+            // If the current div is not the target div, remove the 'big' class
+            currentDiv.classList.remove("big");
+        }
+    }
+}
