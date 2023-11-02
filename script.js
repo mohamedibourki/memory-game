68dd8d23db24e05c3c5564479e0552a86623ddc9
nasasolicode <mohamedibourkioffice@gmail.com>
2023-11-02 17:00:15 +0100

    40%

--- script.js
+++ script.js
@@ -5,5 +5,6 @@ let sit2 = document.querySelectorAll(".sit2")
 x.forEach((e) => {
     e.addEventListener("click", () => {
         document.querySelector(".sit1").setAttribute("id", "vxx")
+        document.querySelector(".sit2").setAttribute("id", "vxx")
     })
 })
