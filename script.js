6638b1957040cf833c6bacc6d7b2f0c90135976f
nasasolicode <mohamedibourkioffice@gmail.com>
2023-11-02 16:58:07 +0100

    40%

---
+++ script.js
@@ -0,0 +1,9 @@
+let x = document.querySelectorAll(".window")
+let sit1 = document.querySelectorAll(".sit1")
+let sit2 = document.querySelectorAll(".sit2")
+
+x.forEach((e) => {
+    e.addEventListener("click", () => {
+        document.querySelector(".sit1").setAttribute("id", "vxx")
+    })
+})
