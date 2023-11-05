633299b88faeec5b31d1e783befba93824cce8b9
mohamedibourki <mohamedibourkioffice@gmail.com>
2023-11-06 00:56:24 +0100

    50%

--- script.js
+++ script.js
@@ -1,35 +1,23 @@
-let sit1 = document.querySelector(".sit1")
-let sit2 = document.querySelector(".sit2")
-let img = document.querySelector("img")
-let big = document.querySelector(".big")
+let wins = document.querySelectorAll('.window');
 
-function myFunc(targetDiv) {
-    var divs = document.querySelectorAll("div");
-
-    var hasBigClass = targetDiv.classList.contains("big");
-
-    for (var i = 0; i < divs.length; i++) {
-        var currentDiv = divs[i];
-        if (currentDiv === targetDiv) {
-            if (hasBigClass) {
-                currentDiv.classList.remove("big");
-                sit1.remove();
-                sit2.remove();
-                img.style.width = "100%"
-            } else {
-                currentDiv.classList.add("big");
-                sit1.style.height = "100%";
-                sit1.style.width = "25%";
-                sit1.style.left = "25%";
-                sit2.style.height = "100%";
-                sit2.style.left = "75%";
-                sit2.style.left = "50%";
-                sit2.style.width = "25%";
-                sit1.classList.add("sat");
-                sit2.classList.add("sot");
-            }
-        } else {
-            currentDiv.classList.remove("big");
-        }
+function toggleActive(win) {
+    if (win.classList.contains('active')) {
+        win.classList.remove('active');
+        win.querySelector('.before').classList.remove('bef');
+        win.querySelector('.after').classList.remove('aft');
+        win.querySelector("img").style.position = "none";
+        win.querySelector("img").style.width = "0%"
+    } else {
+        win.classList.add('active');
+        win.querySelector('.before').classList.add('bef');
+        win.querySelector('.after').classList.add('aft');
+        win.querySelector("img").style.width = "100%"
+        win.querySelector("img").style.position = "absolute";
     }
 }
+
+wins.forEach(win => {
+    win.addEventListener('click', () => {
+        toggleActive(win);
+    });
+});
